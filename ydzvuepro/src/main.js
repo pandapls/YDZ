ce32d6f4f93c311e1d747760a0adf3d08fd1ac36
mygit-hss <3051217870qq.com>
2020-06-03 17:41:54 +0800

    轮播图完成

--- ydzvuepro/src/main.js
+++ ydzvuepro/src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import  axios from 'axios'
-import  {Search} from 'vant'
+import  Vant from 'vant'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import 'vant/lib/index.css'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 //将axios 挂载到Vue上
-Vue.use(Search);
+Vue.use(Vant);
+Vue.use(MintUI)
 new Vue({
   router,
   store,
